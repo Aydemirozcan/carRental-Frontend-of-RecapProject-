215fc63d55014b304a8b000edcd7e18e4260fe92
Aydemir Özcan <aydemir_kanarya@hotmail.com>
2021-03-14 20:38:13 +0300

    17.Day   14.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carrental';
+  title:string = 'carrental';
+  user:string="Aydemir Özcan"
 }
---
+++ src/app/models/customer.ts
@@ -0,0 +1,6 @@
+export interface Customer{
+    customerId:number;
+    customerName:string;
+    companyName:string;
+    userId:number;
+}
